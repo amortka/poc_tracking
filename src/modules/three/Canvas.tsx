32227f7c7a5ba95c7e6bd22c71965d70db7d001a
Marcin Mirecki <markky21@gmail.com>
2020-05-05 21:08:58 +0200

    Initial three components

---
+++ src/modules/three/Canvas.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Canvas as CanvasThree } from 'react-three-fiber'
+import { AmbientLight } from './components/AmbientLight'
+import { Camera } from './components/Camera'
+import { Scene } from './components/Scene'
+import { Floor } from './components/Floor'
+import * as THREE from 'three'
+
+export const Canvas: React.FC = () => {
+  THREE.Object3D.DefaultUp.set(0, 0, 1)
+
+  return (
+    <CanvasThree
+      style={{ background: 'radial-gradient(at 50% 60%, #873740 0%, #272730 40%, #171720 80%, #070710 100%)' }}>
+      <AmbientLight />
+      <Camera />
+      <axesHelper args={[10]} />
+      <Scene>
+        <Floor />
+      </Scene>
+    </CanvasThree>
+  )
+}
