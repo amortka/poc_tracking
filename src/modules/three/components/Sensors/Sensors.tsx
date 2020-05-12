d8e7b7498f7cca46cbb1a70598fe49116b478286
Damian Stolarek (PGS Software) <dstolarek@pgs-soft.com>
2020-05-12 18:46:40 +0200

    Update Sensors rendering

--- src/modules/three/components/Sensors/Sensors.tsx
+++ src/modules/three/components/Sensors/Sensors.tsx
@@ -1,35 +1,20 @@
-import React, { useMemo, useContext } from 'react';
-import { CircleBufferGeometry, MeshBasicMaterial, InstancedMesh, Object3D } from 'three';
-import { ThemeContext } from '../../contexts/ThemeContext';
+import React, { useMemo } from 'react';
 import { IVisualization } from '../../../../models/main.model';
 import { VisualizationType } from '../../canvas.model';
+import { Sensor } from './Sensor';
 
 interface ISensors extends Pick<IVisualization, 'sensors' | 'points'> {
   type: VisualizationType;
 }
 
 export const Sensors: React.FC<ISensors> = ({ points, sensors, type }) => {
-  const theme = useContext(ThemeContext);
+  const sensorModels = useMemo(
+    () =>
+      Object.entries(sensors).map(([sensorId, { point }]) => (
+        <Sensor key={sensorId} position={points[point]} type={type} />
+      )),
+    [points, sensors, type]
+  );
 
-  const instancedSensors = useMemo(() => {
-    let sensorModels = Object.values(sensors);
-
-    var geometry = new CircleBufferGeometry(0.15, 16);
-    var material = new MeshBasicMaterial({ color: theme.sensor[type] });
-
-    const mesh = new InstancedMesh(geometry, material, sensorModels.length);
-
-    const transform = new Object3D();
-    sensorModels.forEach(({ point }, index) => {
-      const pointPosition = points[point];
-      transform.position.set(pointPosition.x, pointPosition.y, 0);
-      transform.updateMatrix();
-
-      mesh.setMatrixAt(index++, transform.matrix);
-    });
-
-    return mesh;
-  }, [points, sensors, theme.sensor, type]);
-
-  return <primitive object={instancedSensors} />;
+  return <>{sensorModels}</>;
 };
