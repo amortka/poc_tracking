1a4d57dca4505ff9253f03566e02d9201dd1e911
Damian Stolarek (PGS Software) <dstolarek@pgs-soft.com>
2020-05-21 07:40:27 +0200

    Vehicle rotation fix

--- src/modules/canvas/components/Vehicle/Vehicle.tsx
+++ src/modules/canvas/components/Vehicle/Vehicle.tsx
@@ -11,13 +11,13 @@ interface IVehicle {
 export const Vehicle: React.FC<IVehicle> = ({ progress, path }) => {
   const position = path.getPoint(progress);
 
-  const geometry = useMemo(() => new BoxBufferGeometry(0.5, 0.25, 0.25), []);
+  const geometry = useMemo(() => new BoxBufferGeometry(0.25, 0.5, 0.25), []);
   const material = useMemo(() => new MeshBasicMaterial({ color: 0xffff00 }), []);
 
   // TODO Predict vehicle direction based on vehicle length and sensor (on vehicle) position
   const ref = useUpdate<THREE.Mesh>(
     (mesh) => {
-      const up = new Vector3(0, 0, 1);
+      const up = new Vector3(0, 1, 0);
       let axis = new Vector3();
       const tangent2d = path.getTangent(progress).normalize();
       const tangent3d = new Vector3(tangent2d.x, tangent2d.y, 0);
