629567f5d4007f3bcc14f430c11b4e9e6519a3a4
Damian Stolarek (PGS Software) <dstolarek@pgs-soft.com>
2020-05-21 07:35:19 +0200

    Update vehicle path animation

--- src/modules/canvas/components/Routes/Route.tsx
+++ src/modules/canvas/components/Routes/Route.tsx
@@ -1,14 +1,64 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { Vector2, Vector3, CurvePath, QuadraticBezierCurve3, QuadraticBezierCurve, Path } from 'three';
 import { RoutePath } from './RoutePath';
 import { IRouteWithComputedData } from '../../canvas.model';
 import { Vehicle } from '../Vehicle/Vehicle';
 
 interface RouteProps extends IRouteWithComputedData {}
 
+const roundedCornerLine = (points: Array<THREE.Vector2>, radius: number = 0.01) => {
+  const minVector = new Vector2();
+  let minLength = minVector.subVectors(points[0], points[1]).length();
+  for (let i = 1; i < points.length - 1; i++) {
+    minLength = Math.min(minLength, minVector.subVectors(points[i], points[i + 1]).length());
+  }
+
+  radius = radius > minLength * 0.5 ? minLength * 0.5 : radius; // radius can't be greater than a half of a minimal segment
+
+  const startIndex = 1;
+  const endIndex = points.length - 2;
+
+  const curvePaths = new CurvePath<THREE.Vector2>();
+  let lastKeyEnd = points[0];
+
+  for (let i = startIndex; i <= endIndex; i++) {
+    const iStart = i - 1 < 0 ? points.length - 1 : i - 1;
+    const iMid = i;
+    const iEnd = i + 1 > points.length - 1 ? 0 : i + 1;
+    const pStart = points[iStart];
+    const pMid = points[iMid];
+    const pEnd = points[iEnd];
+
+    const keyStart = new Vector2().subVectors(pStart, pMid).normalize();
+    const keyMid = pMid;
+    const keyEnd = new Vector2().subVectors(pEnd, pMid).normalize();
+
+    const halfAngle = new Vector3(keyStart.x, keyStart.y, 0).angleTo(new Vector3(keyEnd.x, keyEnd.y, 0)) * 0.5;
+
+    const keyLength = radius / Math.tan(halfAngle);
+
+    keyStart.multiplyScalar(keyLength).add(keyMid);
+    keyEnd.multiplyScalar(keyLength).add(keyMid);
+
+    curvePaths.add(new Path([lastKeyEnd, keyStart]));
+    curvePaths.add(new QuadraticBezierCurve(keyStart, keyMid, keyEnd));
+    lastKeyEnd = keyEnd;
+  }
+
+  const endPath = new Path([curvePaths.curves[curvePaths.curves.length - 1].getPoint(2), points[points.length - 1]]);
+  curvePaths.curves.push(endPath);
+
+  return curvePaths;
+};
+
 export const Route: React.FC<RouteProps> = ({ path, selected, progress }) => {
+  const roundedPath = useMemo(() => {
+    return roundedCornerLine(path.getPoints(), 0.3);
+  }, [path]);
+
   return (
     <>
-      <Vehicle path={path} progress={progress} type={undefined} />
+      <Vehicle path={roundedPath} progress={progress} type={undefined} />
       {selected || true ? (
         <RoutePath distanceEnd={progress} distanceStart={0} color={0x11b572} linewidth={0.007} path={path} />
       ) : null}
--- src/modules/canvas/components/Vehicle/Vehicle.tsx
+++ src/modules/canvas/components/Vehicle/Vehicle.tsx
@@ -1,22 +1,35 @@
 import React, { useMemo } from 'react';
-import { CylinderBufferGeometry, MeshBasicMaterial } from 'three';
+import { CylinderBufferGeometry, MeshBasicMaterial, BoxGeometry, BoxBufferGeometry, Vector3, Object3D } from 'three';
+import { useUpdate } from 'react-three-fiber';
 
 interface IVehicle {
   type: string;
-  path: THREE.Path | THREE.CurvePath<THREE.Vector3>;
+  path: THREE.Path | THREE.CurvePath<THREE.Vector2>;
   progress: number;
 }
 
 export const Vehicle: React.FC<IVehicle> = ({ progress, path }) => {
   const position = path.getPoint(progress);
 
-  // console.log('position', position);
-  // console.log('progress', progress);
-
-  const geometry = useMemo(() => new CylinderBufferGeometry(0.15, 0.15, 0.5, 16), []);
+  const geometry = useMemo(() => new BoxBufferGeometry(0.5, 0.25, 0.25), []);
   const material = useMemo(() => new MeshBasicMaterial({ color: 0xffff00 }), []);
 
   // TODO Predict vehicle direction based on vehicle length and sensor (on vehicle) position
+  const ref = useUpdate<THREE.Mesh>(
+    (mesh) => {
+      const up = new Vector3(0, 0, 1);
+      let axis = new Vector3();
+      const tangent2d = path.getTangent(progress).normalize();
+      const tangent3d = new Vector3(tangent2d.x, tangent2d.y, 0);
 
-  return <mesh args={[geometry, material]} position-x={position.x} position-y={position.y} rotation-x={Math.PI / 2} />;
+      axis = axis.crossVectors(up, tangent3d).normalize();
+
+      const radians = Math.acos(up.dot(tangent3d));
+
+      mesh.quaternion.setFromAxisAngle(axis, radians);
+    },
+    [progress, path]
+  );
+
+  return <mesh ref={ref} args={[geometry, material]} position-x={position.x} position-y={position.y} />;
 };
