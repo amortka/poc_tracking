03cb66454d40ebb8dd0152eafa104297755ac17a
Damian Stolarek (PGS Software) <dstolarek@pgs-soft.com>
2020-05-29 07:59:52 +0200

    Update Vehicle

--- src/modules/canvas/components/Vehicle/Vehicle.tsx
+++ src/modules/canvas/components/Vehicle/Vehicle.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { BoxBufferGeometry, MeshBasicMaterial, Vector3 } from 'three';
+import { BoxBufferGeometry, MeshBasicMaterial, Vector3, Shape, ExtrudeBufferGeometry, Vector2 } from 'three';
 import { useUpdate } from 'react-three-fiber';
 
 interface IVehicle {
@@ -8,9 +8,38 @@ interface IVehicle {
   rotation: THREE.Vector2;
 }
 
+const extrudeSettings = {
+  steps: 1,
+  depth: 0.05,
+  bevelEnabled: true,
+  bevelThickness: 0.05,
+  bevelSize: 0,
+  bevelOffset: 0,
+  bevelSegments: 1,
+};
+
 export const Vehicle: React.FC<IVehicle> = ({ position, rotation }) => {
-  const geometry = useMemo(() => new BoxBufferGeometry(0.25, 0.5, 0.25), []);
-  const material = useMemo(() => new MeshBasicMaterial({ color: 0xffff00, opacity: 0.3, transparent: true }), []);
+  const material = useMemo(() => new MeshBasicMaterial({ color: 0xe9842c }), []);
+
+  const geometry = useMemo(() => {
+    const length = 0.35,
+      width = 0.25;
+
+    const shape = new Shape();
+    shape.moveTo(-width / 2, -length / 2);
+    shape.lineTo(-width / 2, length / 2.5);
+    shape.splineThru([
+      new Vector2(-width / 3, length / 2),
+      new Vector2(width / 3, length / 2),
+      new Vector2(width / 2, length / 2.5),
+    ]);
+    shape.lineTo(width / 2, -length / 2);
+    shape.closePath();
+
+    const geo = new ExtrudeBufferGeometry(shape, extrudeSettings);
+
+    return geo;
+  }, []);
 
   // TODO Predict vehicle direction based on vehicle length and sensor (on vehicle) position
   const ref = useUpdate<THREE.Mesh>(
