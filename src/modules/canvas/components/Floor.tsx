5bd8582f37d25be46a7e58eb44432ccd55408d5d
Marcin Mirecki <markky21@gmail.com>
2020-05-30 13:33:16 +0200

    Fixed major performance bug

--- src/modules/canvas/components/Floor.tsx
+++ src/modules/canvas/components/Floor.tsx
@@ -1,5 +1,5 @@
-import React, { useContext, useMemo } from 'react';
-import { DoubleSide, PlaneBufferGeometry } from 'three';
+import React, { useContext } from 'react';
+import { DoubleSide } from 'three';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { VisualizationType } from '../canvas.model';
 
@@ -7,19 +7,13 @@ interface FloorProps {
   type: VisualizationType;
 }
 
-export const Floor: React.FC<FloorProps> = React.memo(
-  ({ type }) => {
-    const theme = useContext(ThemeContext);
+export const Floor: React.FC<FloorProps> = React.memo(({ type }) => {
+  const theme = useContext(ThemeContext);
 
-    // const geometry = useMemo(() => new PlaneBufferGeometry(100, 100), []);
-
-    console.log('Floor');
-    return (
-      <mesh position-z={-0.1}>
-        <planeBufferGeometry attach="geometry" args={[100, 100]} />
-        <meshBasicMaterial attach="material" side={DoubleSide} color={theme.floor[type]} />
-      </mesh>
-    );
-  },
-  () => true
-);
+  return (
+    <mesh position-z={-0.1}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshBasicMaterial attach="material" side={DoubleSide} color={theme.floor[type]} />
+    </mesh>
+  );
+});
