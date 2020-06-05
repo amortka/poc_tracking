2bf99cc919f001c01a996ff0893f9333045600fe
Marcin Mirecki <markky21@gmail.com>
2020-06-05 08:27:30 +0200

    Fix issue with not initiating CanvasRoute animation

--- src/modules/canvas-route-menager/CanvasRouteManager.tsx
+++ src/modules/canvas-route-menager/CanvasRouteManager.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useSelector } from 'react-redux';
 
 import { Canvas } from '../canvas/Canvas';
@@ -8,13 +8,12 @@ import { useScene } from './hooks/scene.hook';
 import { VehiclesSelectors } from '../../store/vehicles/vehicles.selectors';
 import { VisualizationType } from '../canvas/canvas.model';
 
-const routeIdSet = new Set<string>();
-
 interface CanvasManagerProps {}
 
 export const CanvasRouteManager: React.FC<CanvasManagerProps> = React.memo(() => {
+  const routeIdSet = useRef<Set<string>>(new Set<string>());
   const scene = useScene();
-  const routesState = useRoutesState(routeIdSet);
+  const routesState = useRoutesState(routeIdSet.current);
   const objectsState = useSelector(ObjectsSelectors.objects);
   const vehiclesState = useSelector(VehiclesSelectors.vehicles);
 
