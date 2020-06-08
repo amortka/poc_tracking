30d2d0fd6f891bb56771e74f02ad86b62f3968d1
Marcin Mirecki <markky21@gmail.com>
2020-06-08 15:57:54 +0200

    Fixed displaying float numbers in CardStats component

--- src/modules/ui/components/CartDetails/CardDetailsStats.tsx
+++ src/modules/ui/components/CartDetails/CardDetailsStats.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { GridBoxes } from '../GridBoxes/GridBoxes';
 import { AcUnit, Opacity, Speed, Waves } from '@material-ui/icons';
@@ -9,21 +9,25 @@ const cartDetailsConfig = [
     name: 'Ambient Pressure',
     icon: <Waves />,
     value: null,
+    floatPoint: 0,
   },
   {
     name: 'Humidity',
     icon: <Opacity />,
     value: null,
+    floatPoint: 0,
   },
   {
     name: 'Temperature',
     icon: <AcUnit />,
     value: null,
+    floatPoint: 1,
   },
   {
     name: 'Velocity',
     icon: <Speed />,
     value: null,
+    floatPoint: 2,
   },
 ];
 
@@ -36,6 +40,9 @@ export const StatsCards: React.FC<StatsCardsProps> = React.memo(({ data }) => {
 
   const stats = [ambientPressure, humidity, temperature, velocity];
 
-  const cartDetails = cartDetailsConfig.map((item, i) => ({ ...item, value: stats[i] }));
+  const cartDetails = useMemo(
+    () => cartDetailsConfig.map((item, i) => ({ ...item, value: (+stats[i]).toFixed(item.floatPoint) })),
+    [stats]
+  );
   return <GridBoxes items={cartDetails} dark={true} />;
 });
