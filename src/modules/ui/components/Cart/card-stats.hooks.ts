0b4f1169d1c30d8a1e5f023aff20717e2cc73237
Marcin Mirecki <markky21@gmail.com>
2020-06-10 14:38:25 +0200

    Implement CardStats counters

---
+++ src/modules/ui/components/Cart/card-stats.hooks.ts
@@ -0,0 +1,70 @@
+import { useEffect, useMemo, useState } from 'react';
+import { awaitAfterLoop, progress } from '../../../../mocks/vehicle.mock';
+
+export function useScenarioStepsTime(): number[] {
+  return useMemo(
+    () =>
+      Object.values(progress)
+        .reduce(
+          (accumulator, currentValue, i, array) => {
+            const arrayLength = accumulator.length;
+
+            accumulator[arrayLength - 1] += currentValue.delay;
+
+            if (array[i + 1]?.objectId) {
+              accumulator.push(0);
+            }
+            if (!array[i + 1]) {
+              accumulator[arrayLength - 1] += awaitAfterLoop;
+            }
+            return accumulator;
+          },
+          [0]
+        )
+        .map((v) => v / 1000),
+    []
+  );
+}
+
+export function useScenarioTotalTime(): number {
+  const scenarioStepsTime = useScenarioStepsTime();
+  return useMemo(() => scenarioStepsTime.reduce((accumulator, currentValue) => (accumulator += currentValue), 0), [
+    scenarioStepsTime,
+  ]);
+}
+
+export function useFinishRouteCounter(): number {
+  const totalTime = useScenarioTotalTime();
+  const [displayValue, setDisplayValue] = useState<number>(totalTime);
+
+  useEffect(() => {
+    const idInterval = setInterval(() => {
+      setDisplayValue((current) => (current <= 1 ? totalTime : --current));
+    }, 1000);
+    return () => clearInterval(idInterval);
+  }, [totalTime]);
+
+  return displayValue;
+}
+
+export function useNextStationCounter(): number {
+  const scenarioStepsTime = useScenarioStepsTime();
+  const [displayValue, setDisplayValue] = useState<number>(scenarioStepsTime[0]);
+
+  useEffect(() => {
+    let currentTimeIndex = 0;
+    const idInterval = setInterval(() => {
+      setDisplayValue((current) => {
+        if (current <= 1) {
+          currentTimeIndex = currentTimeIndex === scenarioStepsTime.length - 0.5 ? 0 : currentTimeIndex + 0.5;
+          return scenarioStepsTime[currentTimeIndex];
+        } else {
+          return --current;
+        }
+      });
+    }, 1000);
+    return () => clearInterval(idInterval);
+  }, [scenarioStepsTime]);
+
+  return displayValue;
+}
